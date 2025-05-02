d082d1447e1201939477ec8b7299085bb8eb9dad
airnesygroup <airnesy.info@gmail.com>
2025-05-02 17:31:36 +0300

    Initial commit with new code

--- client/src/components/Patners.jsx
+++ client/src/components/Patners.jsx
@@ -5,9 +5,9 @@ const Partners = () => {
     <div className="overflow-hidden relative w-full h-[40px] md:h-[50px]  border-t border-t-[var(--softBg4)] bg-t-[var(--bg)]
      whitespace-nowrap p-3 md:p-9 fixed  bottom-0 ">
       <div className="flex  gap-9  md:gap-[80px] animate-scroll">
-        <h1 className="text-xl md:text-3xl">Everyone is welcome to the house of the lord</h1>
-        
-        <h1 className="text-xl md:text-3xl">Everyone is welcome to the house of the lord</h1>
+        <h1 className="text-xl md:text-3xl">Everyone is welcome to the house of the lord Everyone is welcome to the house of the lord</h1>
+
+        <h1 className="text-xl md:text-3xl">Everyone is welcome to the house of the lord Everyone is welcome to the house of the lord</h1>
 
       </div>
     </div>
