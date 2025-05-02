0163bfebef5bde7bd2e728c6480368151215c744
airnesygroup <airnesy.info@gmail.com>
2025-05-02 17:47:26 +0300

    Initial commit with new code

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -8,33 +8,33 @@ const Hero = () => {
 
 
   return (
-       <div  className=" bg-cover bg-center h-[70vh] w-full flex flex-col  mt-0 
+       <div  className=" bg-cover bg-center h-[85vh] w-full flex flex-col  mt-0 
        text-white  overflow-y-auto"
     style={{ backgroundImage: "url('/church.gif')" }}>
 
-      <div className="flex flex-col p-3 lg:flex-row lg:h-[60vh] gap-[100px]">
+      <div className="flex flex-col p-3 lg:flex-row gap-[100px]">
         {/* Left Section */}
         <div className="max-w-[500px] flex flex-col mt-4 md:mt-[50px] gap-1 md:gap-4 rounded-md ml-[3px] md:ml-[100px]">
-        <h1 className="text-4xl lg:text-7xl font-bold text-[var(--textColor)]">
+        <h1 className="text-4xl lg:text-7xl font-bold text-white ">
             For God
           </h1>
-          <h1 className="text-4xl lg:text-7xl font-bold text-[var(--textColor)]">
+          <h1 className="text-4xl lg:text-7xl font-bold text-white ">
             For People
           </h1>
-          <h1 className="text-4xl lg:text-7xl font-bold text-[var(--textColor)]">
+          <h1 className="text-4xl lg:text-7xl font-bold text-white ">
             For the City
           </h1>
-          <h1 className="text-4xl lg:text-7xl font-bold text-[var(--textColor)]">
+          <h1 className="text-4xl lg:text-7xl font-bold text-white ">
             For The World
           </h1>
          
-          <p className="text-md md:text-xl text-[var(--textColor)]">
+          <p className="text-md md:text-xl text-white ">
           Welcome to a place where the gospel is central and Jesus is always the lead story.
            We are a Jesus church and want to gather in worship and scatter to shine His light and love throughout the city and beyond.
           </p>
           <Link
             to="/about"
-            className=" max-w-[150px] flex text-center mt-3 md:mt-0 sm:w-auto px-4 md:px-12 py-3 md:py-3 bg-[#0062e3]   text-white font-semibold
+            className=" text-center mt-3 md:mt-0 sm:w-auto px-4 md:px-12 py-3 md:py-3 bg-[#0062e3]   text-white font-semibold
              rounded-full hover:bg-[#0053bf]   "
           >
             Learn More
