e078b4cf4fe7d75dd274fba5871d29f123cecb42
airnesygroup <airnesy.info@gmail.com>
2025-05-04 20:08:11 +0300

    Initial commit with new code

--- client/src/components/Images.jsx
+++ client/src/components/Images.jsx
@@ -8,41 +8,47 @@ const Images = () => {
   return (
     <div className="overflow-hidden w-full">
       <div className="flex animate-scroll-left gap-4 w-max">
-        {images.map((src, index) => {
-          const isSquare = index % 2 === 0;
-          return (
-            <div
-              key={index}
-              className={`flex-shrink-0 rounded-lg bg-gray-100 ${
-                isSquare ? "aspect-[3/5] w-48" : "aspect-[3/3] h-48 w-48"
-              } overflow-hidden`}
-            >
-              <img
-                src={src}
-                alt={`Image ${index + 1}`}
-                className="w-full h-full object-cover rounded-lg"
-              />
-            </div>
-          );
-        })}
+      {images.map((src, index) => {
+  const isSquare = index % 2 === 0;
+  return (
+    <div
+      key={index}
+      className={`flex-shrink-0 rounded-lg bg-gray-100 ${
+        isSquare ? "aspect-[3/4] w-50" : "aspect-[3/4] w-50 flex items-center"
+      } overflow-hidden`}
+    >
+      <div className={`${!isSquare ? "aspect-[3/3] w-full" : "w-full h-full"}`}>
+        <img
+          src={src}
+          alt={`Image ${index + 1}`}
+          className="w-full h-full object-cover rounded-lg"
+        />
+      </div>
+    </div>
+  );
+})}
+
         {/* Duplicate for seamless scroll */}
         {images.map((src, index) => {
-          const isSquare = index % 2 === 0;
-          return (
-            <div
-              key={`dup-${index}`}
-              className={`flex-shrink-0 rounded-lg bg-gray-100 ${
-                isSquare ? "aspect-[3/5] w-48" : "aspect-[3/3] h-48 w-48"
-              } overflow-hidden`}
-            >
-              <img
-                src={src}
-                alt={`Image ${index + 1}`}
-                className="w-full h-full object-cover rounded-lg"
-              />
-            </div>
-          );
-        })}
+  const isSquare = index % 2 === 0;
+  return (
+    <div
+      key={index}
+      className={`flex-shrink-0 rounded-lg bg-gray-100 ${
+        isSquare ? "aspect-[3/4] w-50" : "aspect-[3/4] w-50 flex items-center"
+      } overflow-hidden`}
+    >
+      <div className={`${!isSquare ? "aspect-[3/3] w-full" : "w-full h-full"}`}>
+        <img
+          src={src}
+          alt={`Image ${index + 1}`}
+          className="w-full h-full object-cover rounded-lg"
+        />
+      </div>
+    </div>
+  );
+})}
+
       </div>
     </div>
   );
