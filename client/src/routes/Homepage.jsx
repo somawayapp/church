c26dc46a796ea4ad8d0ea9488dda0f310f265b01
airnesygroup <airnesy.info@gmail.com>
2025-05-04 22:16:42 +0300

    Initial commit with new code

--- client/src/routes/Homepage.jsx
+++ client/src/routes/Homepage.jsx
@@ -208,8 +208,8 @@ We value the presence of God. Our worship, preaching and teaching, and Children'
     className="absolute top-0 left-0 w-full h-24 z-10"
     style={{ transform: "rotate(180deg)" }}
   >
-    <path fill="var(--bg)" d="M0,0 C 480,225 975,-180 1440,32 L1440,0 L0,0 Z" />
-    <path fill="var(--bg)" d="M0,0 C 480,305 975,-180 1440,96 L1440,0 L0,0 Z" />
+    <path fill="var(--bd)" d="M0,0 C 480,225 975,-180 1440,32 L1440,0 L0,0 Z" />
+    <path fill="var(--bd)" d="M0,0 C 480,305 975,-180 1440,96 L1440,0 L0,0 Z" />
   </svg>
 </div>
 
