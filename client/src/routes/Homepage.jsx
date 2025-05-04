e2702b6b7778282c2241cbe45d05ad2f2e72872b
airnesygroup <airnesy.info@gmail.com>
2025-05-04 18:59:42 +0300

    Initial commit with new code

--- client/src/routes/Homepage.jsx
+++ client/src/routes/Homepage.jsx
@@ -147,10 +147,9 @@ const Homepage = () => {
 
 <div>
 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none" class="absolute top-0 left-0 w-full h-24 z-10">
-  <path fill="#4c1d95" d="M0,26 C 480,130 960,-40 1440,32 L1440,0 L0,0 Z"></path>
-  <path fill="#4c1d95" d="M0,26 C 480,210 960,0 1440,96 L1440,0 L0,0 Z"></path>
+  <path fill="#4c1d95" d="M0,0 C 480,200 960,-40 1440,32 L1440,0 L0,0 Z"></path>
+  <path fill="#4c1d95" d="M0,0 C 480,280 960,0 1440,126 L1440,0 L0,0 Z"></path>
 </svg>
-
 
 
 
