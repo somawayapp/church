f227f958555571aa2c7bdf79c8254ad4be41e5e6
airnesygroup <airnesy.info@gmail.com>
2025-05-09 21:25:22 +0300

    Initial commit with new code

--- client/src/routes/AboutPage.jsx
+++ client/src/routes/AboutPage.jsx
@@ -331,9 +331,15 @@ We value the presence of God. Our worship, preaching and teaching, and Children'
  <div className=" order-1 md:order-2 block w-full  rounded-lg md:rounded-[20px] md:w-1/2 mx-auto md:mx-0"  data-aos="fade-up"
   data-aos-delay="300"> 
 
-<div className="md:w-1/2 ">
-<img src="/pics2.jpg" className=" h-[500px] rounded-full transform hover:scale-105 transition-all duration-300 object-cover" />
+<div className="flex justify-end">
+  <div className="md:w-1/2">
+    <img
+      src="/pics2.jpg"
+      className="h-[500px] rounded-full transform hover:scale-105 transition-all duration-300 object-cover"
+    />
+  </div>
 </div>
+
 
   </div>
 </div>
