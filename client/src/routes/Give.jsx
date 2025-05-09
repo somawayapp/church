012867f730ecd8ceaeeca6631380404d6ce5ad3d
airnesygroup <airnesy.info@gmail.com>
2025-05-09 23:23:22 +0300

    Initial commit with new code

--- client/src/routes/Give.jsx
+++ client/src/routes/Give.jsx
@@ -168,8 +168,8 @@ No matter your stage of life, we want you to be part of a Christ-centered commun
 
   {/* Text overlay */}
   <div className="absolute inset-0 flex items-center justify-center">
-    <h2 className="text-white text-2xl md:text-3xl font-bold  px-4 py-2 rounded">
-      Send money <br /><br />
+    <h2 className="text-white text-4xl md:text-6xl font-bold  px-4 py-2 rounded">
+      Send money <br /><br /> <br /><br /><br /><br />
       0703394794
     </h2>
   </div>
