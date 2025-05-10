82a11f94a1591e5047598394a973deb8dcf18373
airnesygroup <airnesy.info@gmail.com>
2025-05-10 11:31:14 +0300

    Initial commit with new code

--- client/src/routes/Gallery.jsx
+++ client/src/routes/Gallery.jsx
@@ -117,7 +117,7 @@ const Gallery = () => {
         <div
           key={index}
           className={`flex-shrink-0 rounded-lg overflow-hidden relative ${
-            isSquare ? "aspect-[3/4] w-20 md:w-60" : "aspect-[3/4] w-20 md:w-60"
+            isSquare ? "aspect-[3/4] w-24 md:w-60" : "aspect-[3/4] w-24 md:w-60"
           }`}
         >
           <div
@@ -129,7 +129,7 @@ const Gallery = () => {
               src={src}
               alt={`Image ${index + 1}`}
               className={`object-cover rounded-lg transform hover:scale-105 transition-all duration-300 ${
-                isSquare ? "w-full h-full" : "w-20 h-20 md:w-60 md:h-60"
+                isSquare ? "w-full h-full" : "w-24 h-24 md:w-60 md:h-60"
               }`}
             />
           </div>
